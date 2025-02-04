77cef0a9cefbf210d0723915eab44f6ccf1c7d18
PREZGV <116239815+PREZGV@users.noreply.github.com>
2025-02-04 14:01:33 -0600

    fixed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
       <h2 className="text-4xl font-bold text-gray-800">My Resume</h2>
       <p className="mt-4 text-lg text-gray-600">View my resume below:</p>
       <a
-        href="/resume.pdf" // This should match the actual file name in the public folder
+        href={`${process.env.PUBLIC_URL}/resume.pdf`} // Ensure this matches the actual file name in the public folder
         target="_blank"
         rel="noopener noreferrer"
         className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
